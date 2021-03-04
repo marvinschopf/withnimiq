30a9866da98e2050c4941beca6ee504b034ce98a
Marvin Schopf <marvin@schopf.biz>
2021-03-04 21:04:40 +0100

    Fix navbar

--- src/BaseLayout.tsx
+++ src/BaseLayout.tsx
@@ -1,5 +1,5 @@
 import * as React from "preact";
-import { Link } from "gatsby";
+import { navigate } from "gatsby";
 
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -9,15 +9,23 @@ export default class BaseLayout extends React.Component {
 		return (
 			<React.Fragment>
 				<Navbar>
-					<Link to="/">
-						<Navbar.Brand>WithNimiq</Navbar.Brand>
-					</Link>
+					<Navbar.Brand
+						onClick={() => {
+							navigate("/");
+						}}
+					>
+						WithNimiq
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="mr-auto">
-							<Link to="/">
-								<Nav.Link>Home</Nav.Link>
-							</Link>
+							<Nav.Link
+								onClick={() => {
+									navigate("/");
+								}}
+							>
+								Home
+							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Navbar>

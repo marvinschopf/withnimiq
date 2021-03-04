0966d3d80eba801bb38d27eb0d6d66057a5b396a
Marvin Schopf <marvin@schopf.biz>
2021-03-04 21:32:15 +0100

    Add dark theme

--- src/BaseLayout.tsx
+++ src/BaseLayout.tsx
@@ -1,36 +1,45 @@
 import * as React from "preact";
 import { navigate } from "gatsby";
+import { useMediaPredicate } from "preact-media-hook";
 
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
-export default class BaseLayout extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Navbar>
-					<Navbar.Brand
-						onClick={() => {
-							navigate("/");
-						}}
-					>
-						WithNimiq
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="mr-auto">
-							<Nav.Link
-								onClick={() => {
-									navigate("/");
-								}}
-							>
-								Home
-							</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-				{this.props.children}
-			</React.Fragment>
-		);
-	}
-}
+type BaseLayoutProps = {};
+
+const BaseLayout: React.FunctionalComponent<BaseLayoutProps> = function (
+	props
+) {
+	const systemThemeDark: boolean = useMediaPredicate(
+		"(prefers-color-scheme: dark)"
+	);
+
+	return (
+		<React.Fragment>
+			<Navbar bg={systemThemeDark ? "dark" : "light"}>
+				<Navbar.Brand
+					onClick={() => {
+						navigate("/");
+					}}
+				>
+					WithNimiq
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link
+							onClick={() => {
+								navigate("/");
+							}}
+						>
+							Home
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			{props.children}
+		</React.Fragment>
+	);
+};
+
+export default BaseLayout;

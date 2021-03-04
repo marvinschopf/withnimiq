752c77be5fb464cc7b3a741605cdee36d1aa2062
Marvin Schopf <marvin@schopf.biz>
2021-03-04 16:16:45 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Nimiq Apps",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};

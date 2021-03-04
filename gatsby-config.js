9fad48ab71974a51615d026a77b20ee9499114d1
Marvin Schopf <marvin@schopf.biz>
2021-03-04 16:47:26 +0100

    Run prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  siteMetadata: {
-    title: "Nimiq Apps",
-    siteUrl: "https://withnimiq.vercel.app",
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-  ],
+	siteMetadata: {
+		title: "Nimiq Apps",
+		siteUrl: "https://withnimiq.vercel.app",
+	},
+	plugins: [
+		"gatsby-plugin-sass",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap",
+	],
 };
